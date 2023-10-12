8d3894567cd0fdf2233eea8b64c65507d1e56f7c
AGiljanovic <agiljano@xin720948.speedport.ip>
2023-10-12 19:39:03 +0200

    Add token validation with regex

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -5,12 +5,16 @@ export const verifyToken = async (req, res, next) => {
     let token = req.header("Authorization");
 
     if (!token) {
-      return res.status(401).json({ error: "Unauthorized" });
+      return res.status(401).json({ error: "Unauthorized: Token Missing" });
     }
 
-    if (token.startsWith("Bearer ")) {
-      token = token.slice(7, token.length).trimLeft();
+    const bearerTokenPattern = /^Bearer\s[a-zA-Z0-9\-_]+\.[a-zA-Z0-9\-_]+\.[a-zA-Z0-9\-_]+$/;
+
+    if (!bearerTokenPattern.test(token)) {
+      return res.status(401).json({ error: "Invalid Token Format" });
     }
+
+    token = token.split(' ')[1];
 
     const verified = jwt.verify(token, process.env.JWT_SECRET);
     req.user = verified;
