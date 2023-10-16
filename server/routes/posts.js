44063c67fc53bd2111c1a6b8377b716086dfd856
AGiljanovic <agiljano@xin720948.speedport.ip>
2023-10-16 23:18:50 +0200

    Add Validation To Post Routes

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,14 +1,34 @@
 import express from "express";
+import Joi from 'joi';
 import { getFeedPosts, getUserPosts, likePost } from "../controllers/posts.js";
 import { verifyToken } from "../middleware/auth.js";
 
 const router = express.Router();
 
+const userIdSchema = Joi.string().required();
+const postIdSchema = Joi.string().required();
+const likePostSchema = Joi.object({
+    like: Joi.boolean().required(),
+});
+
 /* 👓 Read 👓 */
 router.get("/", verifyToken, getFeedPosts);
-router.get("/:userId/posts", verifyToken, getUserPosts);
+
+router.get("/:userId/posts", verifyToken, async (req, res, next) => {
+    const { error } = userIdSchema.validate(req.params.userId);
+    if (error) return res.status(400).json({ error: error.details[0].message });
+    return next();
+}, getUserPosts);
 
 /* 🔄 Update 🔄 */
-router.patch("/:id/like", verifyToken, likePost);
+router.patch("/:id/like", verifyToken, async (req, res, next) => {
+    const { error } = postIdSchema.validate(req.params.id);
+    if (error) return res.status(400).json({ error: error.details[0].message });
+    return next();
+}, async (req, res, next) => {
+    const { error } = likePostSchema.validate(req.body);
+    if (error) return res.status(400).json({ error: error.details[0].message });
+    return next();
+}, likePost);
 
 export default router;
