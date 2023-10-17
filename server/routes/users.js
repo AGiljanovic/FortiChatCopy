10d735f2674438e4ebe15a2f568f4a10605893e4
AGiljanovic <agiljano@xin720948.speedport.ip>
2023-10-17 21:09:48 +0200

    Refractor User Routes Joi Validation & Error Handling

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,6 +1,7 @@
 import express from "express";
 import morgan from 'morgan';
-import Joi from 'joi';
+import joi from 'joi';
+import joiObjectId from 'joi-objectid';
 
 import { getUser, getUserFriends, addRemoveFriend } from "../controllers/users.js";
 import { verifyToken } from "../middleware/auth.js";
@@ -13,30 +14,47 @@ const router = express.Router();
 router.use(morgan('combined'));
 router.use(apiRateLimiter);
 
-const userIdSchema = Joi.string().required();
-const friendIdSchema = Joi.string().required();
+joi.objectId = joiObjectId(joi);
+
+const userIdSchema = joi.objectId().required();
+const friendIdSchema = joi.objectId().required();
 
 /* 👓 Read 👓 */
 router.get("/:id", verifyToken, async (req, res, next) => {
-    const { error } = userIdSchema.validate(req.params.id);
-    if (error) return res.status(400).json({ error: "Invalid input provided" });
-    return next();
+    try {
+        const { error } = userIdSchema.validate(req.params.id);
+        if (error) throw new Error("Invalid user ID.");
+        return next();
+    } catch(err) {
+        next(err);
+    }
 }, getUser);
 
 router.get("/:id/friends", verifyToken, async (req, res, next) => {
-    const { error } = userIdSchema.validate(req.params.id);
-    if (error) return res.status(400).json({ error: "Invalid input provided" });
-    return next();
+    try {
+        const { error } = userIdSchema.validate(req.params.id);
+        if (error) throw new Error("Invalid user ID.");
+        return next();
+    } catch(err) {
+        next(err);
+    }
 }, getUserFriends);
 
 /* 🔄 Update 🔄 */
 router.patch("/:id/:friendId", verifyToken, async (req, res, next) => {
-    const { error: errorUserId } = userIdSchema.validate(req.params.id);
-    const { error: errorFriendId } = friendIdSchema.validate(req.params.friendId);
-
-    if (errorUserId || errorFriendId) return res.status(400).json({ error: "Invalid input provided" });
-
-    return next();
+    try {
+        const { error: errorUserId } = userIdSchema.validate(req.params.id);
+        const { error: errorFriendId } = friendIdSchema.validate(req.params.friendId);
+        if (errorUserId || errorFriendId) throw new Error("Invalid ID(s) provided.");
+        return next();
+    } catch(err) {
+        next(err);
+    }
 }, addRemoveFriend);
+
+router.use((error, req, res, next) => {
+    console.error(error.stack);
+    res.status(500).json({ error: "An internal error occurred." });
+});
 
 export default router;
