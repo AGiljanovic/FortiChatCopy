dcfdaf24577565535ffe76e66c505138b164727b
AGiljanovic <agiljano@xin720948.speedport.ip>
2023-10-17 20:31:42 +0200

    Refractor Auth Routes - Add Validation & Error Handling

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,12 +1,28 @@
 import express from "express";
-
+import Joi from "joi";
 import { login } from "../controllers/auth.js";
 import { loginLimiter } from "../middleware/rateLimiter.js";
 
-
 const router = express.Router();
 
-/* 👤 User Login Route (with Rate Limiter) 👤 */
-router.post("/login", validate(loginSchema), loginLimiter, login);
+router.use(loginLimiter);
+
+const loginSchema = Joi.object({
+    email: Joi.string().email().required(),
+    password: Joi.string().required()
+});
+
+const validate = (schema) => {
+    return (req, res, next) => {
+        const { error } = schema.validate(req.body);
+        if (error) {
+            return res.status(400).json({ error: "Invalid input data. Please check and try again." });
+        }
+        next();
+    };
+};
+
+/* 👤 User Login Route 👤 */
+router.post("/login", validate(loginSchema), login);
 
 export default router;
