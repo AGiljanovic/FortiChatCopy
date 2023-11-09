ab79761d39c1dc1afa022cf9c5bc9f27d172a099
AGiljanovic <agiljano@xin720948.speedport.ip>
2023-11-09 21:01:48 +0100

    Update controllers and routes of users

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,90 +1,134 @@
 import mongoose from "mongoose";
+import User from "../models/user.js";
 import sanitize from 'mongo-sanitize';
 
-import User from "../models/user.js";
+/* ✅ Checker for Valid MongoDB ObjectId ✅ */
+const isValidObjectId = (id) => mongoose.Types.ObjectId.isValid(id);
 
-/* 👓 Read: Get User Info 👓 */
+/* 👓 Read 👓 */
 export const getUser = async (req, res) => {
   try {
-    const { id } = req.params;
-    const sanitizedId = sanitize(id);
-    const user = await User.findById(sanitizedId);
+    const sanitizedId = sanitize(req.params.id);
+    if (!isValidObjectId(sanitizedId)) {
+      return res.status(400).json({ message: "Invalid user ID format." });
+    }
+
+    const user = await User.findById(sanitizedId).select('-password -otherSensitiveField');
+    if (!user) {
+      return res.status(404).json({ message: "User not found." });
+    }
     res.status(200).json(user);
   } catch (err) {
-    console.error(err);
-    res.status(500).json({ message: "An error occurred while retrieving the user." });
+    res.status(500).json({ message: "Internal server error." });
   }
 };
 
-/* 📖  Read: Get User Friends 📖 */
+/* 👯‍♀️ Read User Friends 👯‍♀️ */
 export const getUserFriends = async (req, res) => {
   try {
-    const { id } = req.params;
-    const sanitizedId = sanitize(id);
+    const sanitizedId = sanitize(req.params.id);
+    if (!isValidObjectId(sanitizedId)) {
+      return res.status(400).json({ message: "Invalid user ID format." });
+    }
+
     const user = await User.findById(sanitizedId);
+    if (!user) {
+      return res.status(404).json({ message: "User not found." });
+    }
 
     const friends = await Promise.all(
-      user.friends.map((friendId) => User.findById(friendId))
+      user.friends.map((friendId) => {
+        const sanitizedFriendId = sanitize(friendId);
+        if (!isValidObjectId(sanitizedFriendId)) {
+          throw new Error('Invalid friend ID format.');
+        }
+        return User.findById(sanitizedFriendId).select('_id firstName lastName occupation location picturePath');
+      })
     );
-    
-    const formattedFriends = friends.map(
-      ({ _id, firstName, lastName, occupation, location, picturePath }) => {
-        return { _id, firstName, lastName, occupation, location, picturePath };
-      }
-    );
-    
+    const formattedFriends = friends.map(friend => ({
+      _id: friend._id,
+      firstName: friend.firstName,
+      lastName: friend.lastName,
+      occupation: friend.occupation,
+      location: friend.location,
+      picturePath: friend.picturePath
+    }));
+
     res.status(200).json(formattedFriends);
   } catch (err) {
-    console.error(err);
-    res.status(500).json({ message: "An error occurred while retrieving friends." });
+    res.status(500).json({ message: "Internal server error." });
   }
 };
 
-/* ➕🔄➖ Update: Add or Remove Friend ➕🔄➖ */
+/* 🔁 Update 🔁 */
 export const addRemoveFriend = async (req, res) => {
   const session = await mongoose.startSession();
   session.startTransaction();
-  
+
   try {
-    const { id, friendId } = req.params;
-    const sanitizedId = sanitize(id);
-    const sanitizedFriendId = sanitize(friendId);
-    
-    if (sanitizedId === sanitizedFriendId) throw new Error("User ID and friend ID cannot be the same.");
-    
-    const user = await User.findById(sanitizedId);
-    const friend = await User.findById(sanitizedFriendId);
-    
-    if (!user || !friend) throw new Error();
-    
-    if (user.friends.includes(sanitizedFriendId)) {
-      user.friends = user.friends.filter((fid) => fid !== sanitizedFriendId);
-      friend.friends = friend.friends.filter((fid) => fid !== sanitizedId);
+    const sanitizedId = sanitize(req.params.id);
+    const sanitizedFriendId = sanitize(req.params.friendId);
+
+    if (!isValidObjectId(sanitizedId) || !isValidObjectId(sanitizedFriendId)) {
+      await session.abortTransaction();
+      return res.status(400).json({ message: "Invalid user ID or friend ID format." });
+    }
+
+    if (sanitizedId === sanitizedFriendId) {
+      await session.abortTransaction();
+      return res.status(400).json({ message: "Users cannot befriend themselves." });
+    }
+
+    const user = await User.findById(sanitizedId).session(session);
+    const friend = await User.findById(sanitizedFriendId).session(session);
+
+    if (!user || !friend) {
+      await session.abortTransaction();
+      return res.status(404).json({ message: "User or friend not found." });
+    }
+
+    const userIndex = user.friends.indexOf(sanitizedFriendId);
+    const friendIndex = friend.friends.indexOf(sanitizedId);
+
+    if (userIndex > -1) {
+      user.friends.splice(userIndex, 1);
+      friend.friends.splice(friendIndex, 1);
     } else {
       user.friends.push(sanitizedFriendId);
       friend.friends.push(sanitizedId);
     }
-    
+
     await user.save({ session });
     await friend.save({ session });
-    
+
     await session.commitTransaction();
     session.endSession();
-    
-    const friends = await Promise.all(user.friends.map((id) => User.findById(id)));
-    const formattedFriends = friends.map(
-      ({ _id, firstName, lastName, occupation, location, picturePath }) => {
-        return { _id, firstName, lastName, occupation, location, picturePath };
-      }
+
+    // Log Success of Addition or Removal of Friend 
+    console.log(`User ${sanitizedId} updated friends list: ${sanitizedFriendId} ${user.friends.includes(sanitizedFriendId) ? 'added' : 'removed'}.`);
+
+    const updatedFriends = await Promise.all(
+      user.friends.map(friendId => User.findById(friendId).select('_id firstName lastName occupation location picturePath'))
     );
-    
+
+    const formattedFriends = updatedFriends.map(friend => ({
+      _id: friend._id,
+      firstName: friend.firstName,
+      lastName: friend.lastName,
+      occupation: friend.occupation,
+      location: friend.location,
+      picturePath: friend.picturePath
+    }));
+
     res.status(200).json(formattedFriends);
-    
   } catch (err) {
     await session.abortTransaction();
     session.endSession();
-    console.error(err);
-    
-    res.status(500).json({ message: "An error occurred while updating friend list." });
-  }
+    if (err.kind === 'ObjectId') {
+        // Id Not a Valid MongoDB ObjectId
+        res.status(400).json({ message: "Invalid user ID or friend ID." });
+    } else {
+        res.status(500).json({ message: "Internal server error." });
+    }
+}
 };
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,60 +1,19 @@
 import express from "express";
-import morgan from 'morgan';
-import joi from 'joi';
-import joiObjectId from 'joi-objectid';
-
 import { getUser, getUserFriends, addRemoveFriend } from "../controllers/users.js";
 import { verifyToken } from "../middleware/auth.js";
+import { validateUserParams } from "../middleware/validation.js"; 
 import { apiRateLimiter } from '../middleware/rateLimiter.js';
-
 
 const router = express.Router();
 
-/* 👷‍♀️ Middlewares 👷‍♀️ */
-router.use(morgan('combined'));
+/* Rate limiter */
 router.use(apiRateLimiter);
 
-joi.objectId = joiObjectId(joi);
+/* READ */
+router.get("/:id", verifyToken, validateUserParams, getUser);
+router.get("/:id/friends", verifyToken, validateUserParams, getUserFriends);
 
-const userIdSchema = joi.objectId().required();
-const friendIdSchema = joi.objectId().required();
-
-/* 👓 Read 👓 */
-router.get("/:id", verifyToken, async (req, res, next) => {
-    try {
-        const { error } = userIdSchema.validate(req.params.id);
-        if (error) throw new Error("Invalid user ID.");
-        return next();
-    } catch(err) {
-        next(err);
-    }
-}, getUser);
-
-router.get("/:id/friends", verifyToken, async (req, res, next) => {
-    try {
-        const { error } = userIdSchema.validate(req.params.id);
-        if (error) throw new Error("Invalid user ID.");
-        return next();
-    } catch(err) {
-        next(err);
-    }
-}, getUserFriends);
-
-/* 🔄 Update 🔄 */
-router.patch("/:id/:friendId", verifyToken, async (req, res, next) => {
-    try {
-        const { error: errorUserId } = userIdSchema.validate(req.params.id);
-        const { error: errorFriendId } = friendIdSchema.validate(req.params.friendId);
-        if (errorUserId || errorFriendId) throw new Error("Invalid ID(s) provided.");
-        return next();
-    } catch(err) {
-        next(err);
-    }
-}, addRemoveFriend);
-
-router.use((error, req, res, next) => {
-    console.error(error.stack); // Reminder: Remove once I am done -> Security risk
-    res.status(500).json({ error: "An internal error occurred." });
-});
+/* UPDATE */
+router.patch("/:id/:friendId", verifyToken, validateUserParams, addRemoveFriend);
 
 export default router;
